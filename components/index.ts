8b3881df0f4a355e96816ffdac9f7f0391c657b4
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-08-09 19:26:06 +0300

    add loader

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,1 @@
-export { Header } from "./ui";
+export { Header, Loader } from "./ui";
---
+++ components/ui/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <span className="loading loading-spinner loading-lg" />
+    </div>
+  );
+};
